6a2293199ff1bc9f1d8d04d32d6b826fc79ac629
sohyeonAn <ansh4393@gmail.com>
2022-02-17 16:43:28 +0900

    🎨 Editor 컴포넌트 추가

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import Responsive from '../components/common/Responsive';
+import Editor from '../components/write/Editor';
+import TagBox from '../components/write/TagBox';
+import WriteActionButtons from '../components/write/WriteActionButtons';
 
 const WritePage = () => {
-  return <div>WritePage</div>;
+  return (
+    <Responsive>
+      <Editor />
+      <TagBox />
+      <WriteActionButtons />
+    </Responsive>
+  );
 };
 
 export default WritePage;
