d2b7a1dcacb62d9df09aac50a39121c8c7d0a8e7
sohyeonAn <ansh4393@gmail.com>
2022-02-18 23:08:23 +0900

    🎨 meta title설정

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import PostListPage from './pages/PostListPage';
 import PostPage from './pages/PostPage';
 import RegisterPage from './pages/RegisterPage';
 import WritePage from './pages/WritePage';
+import { Helmet } from 'react-helmet-async';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<PostListPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/write" element={<WritePage />} />
-      <Route path="/@:username">
-        <Route index element={<PostListPage />} />
-        <Route path=":postId" element={<PostPage />} />
-      </Route>
-    </Routes>
+    <>
+      <Helmet>
+        <title>REACTERS</title>
+      </Helmet>
+      <Routes>
+        <Route path="/" element={<PostListPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="/@:username">
+          <Route index element={<PostListPage />} />
+          <Route path=":postId" element={<PostPage />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
